744dab3657a9633b517d989674738e6e70330990
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-03-04 03:23:00 -0600

    implement basic character controller 2d and character data

---
+++ Assets/Scripts/Character/CharacterController2D.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D: MonoBehaviour
+{
+    [Header("Movement Settings")]
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+    public CharacterData data;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // Get the horizontal and vertical input axes
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (horizontalInput > 0)
+            spriteRenderer.flipX = false;
+        else if (horizontalInput < 0)
+            spriteRenderer.flipX = true;
+
+        // Calculate the movement vector
+        movement = new Vector2(horizontalInput, verticalInput).normalized * data.moveSpeed;
+        if (horizontalInput!=0 || verticalInput!=0)
+        {
+            animator.SetBool("isMove", true);
+            animator.SetFloat("speed", movement.x+ movement.y);
+
+        }
+        else
+        {
+            animator.SetBool("isMove", false);
+
+        }
+
+
+    }
+
+
+
+
+    private void FixedUpdate()
+    {
+        // Apply the movement vector to the character's Rigidbody2D component
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Character/CharacterData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "Data/CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    [MinValue(0)]
+    [GUIColor(0.2f, 0.2f, 1f)]
+    public float moveSpeed = 5f;
+    [MinValue(0)]
+    [GUIColor(0, 1, 0)]
+    public int health = 3;
+    [MinValue(0.1)]
+    public int racketSize = 1;
+    [MinValue(0)]
+    public int racketDamage = 1;
+    [MinValue(0.1)]
+    public int racketSpeed = 1;
+    [MinValue(0.1)]
+    public int racketRange = 1;
+
+
+
+}
