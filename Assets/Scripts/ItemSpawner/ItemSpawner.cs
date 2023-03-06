97b352d77e2dc1155bc1ba4a2618d0aff8494d70
Jacky Park <jackypark9852@gmail.com>
2023-03-06 07:34:19 -0600

    Add ItemPoolData to WaveManager

--- Assets/Scripts/ItemSpawner/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner/ItemSpawner.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class ItemSpawner : MonoBehaviour
 {
-    [Header("Data")]
-    [Tooltip("This data should contain a list of items and their weights. The weights determine the probability of an item being selected.")]
-    [Required]
-    [AssetsOnly]
-    [SerializeField] private ItemPoolData _itemPoolData;
-    [Space(10)]
-
     [Header("Item Spawn Settings")]
     [ReadOnly]
     [ShowInInspector]
     private int _itemCount = 0;
     [SerializeField] private List<Vector2> _spawnOffsets = new List<Vector2>(); // Stores local positions of the spawn anchors
+
+    private WaveManager _waveManager;
 
 
     private void OnValidate() // Called when a value is changed in the inspector
@@ -28,10 +23,16 @@ public class ItemSpawner : MonoBehaviour
     {
         Enemy enemy = GetComponent<Enemy>();
         enemy.died.AddListener(() => SpawnItems());
+        _waveManager = FindObjectOfType<WaveManager>();
+        if (_waveManager == null)
+        {
+            Debug.LogError("No WaveManager found in scene, disabling ItemSpawner");
+            enabled = false;
+        }
     }
     private void SpawnItems()
     {
-        List<GameObject> items = _itemPoolData.GetRandomItems(_itemCount);
+        List<GameObject> items = _waveManager.ItemPoolData.GetRandomItems(_itemCount);
         for (int i = 0; i < _itemCount; i++)
         {
             Instantiate(items[i], transform.position + new Vector3(_spawnOffsets[i].x, _spawnOffsets[i].y, 0), Quaternion.identity);
