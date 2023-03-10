238123271b6bee935a7580516df8d6603c89b2f5
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-03-10 01:26:26 -0600

    implement enemy death effect

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
-
+using DG.Tweening;
 public class Enemy : Entity
 {
     public EnemyData enemyData;
@@ -19,10 +19,13 @@ public class Enemy : Entity
     private float dashCooldownTimer = 0;
     [SerializeField]
     private float attackCooldownTimer = 0;
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
     private void Awake()
     {
         _target = GameObject.FindGameObjectWithTag("Player");
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         if (_target == null)
             Debug.LogAssertion("Target is null");
         if (enemyData == null)
@@ -53,7 +56,7 @@ public class Enemy : Entity
     private void Update()
     {
         //Cooldown timer for attack
-        attackCooldownTimer = (float)behaviorTree.GetVariable("attackCooldownTimer").GetValue();
+        attackCooldownTimer = (float)behaviorTree.GetVariable("attackCooldownTimer")?.GetValue();
         if (attackCooldownTimer > 0)
         {
             attackCooldownTimer -= Time.deltaTime;
@@ -63,7 +66,7 @@ public class Enemy : Entity
         switch (enemyData.enemyType)
         {
             case EnemyType.dasher:
-            dashCooldownTimer = (float)behaviorTree.GetVariable("dashCooldownTimer").GetValue();
+            dashCooldownTimer = (float)behaviorTree.GetVariable("dashCooldownTimer")?.GetValue();
                 if (dashCooldownTimer > 0)
                 {
                     dashCooldownTimer -= Time.deltaTime;
@@ -75,34 +78,42 @@ public class Enemy : Entity
     }
 
     [Button("Kill")]
-    public void Kill()
+    public void Kill(Vector3 direction = default)
     {
         died.Invoke();
         behaviorTree.enabled = false;
         _animator.Play("Die");
-        //Destroy(gameObject) after die animation is done
+        //Death Effect
+        transform.DOJump(transform.position - direction, 0.5f, 1, 0.5f);
+        _spriteRenderer.DOFade(0.4f, 0.1f)
+                .SetLoops(3, LoopType.Yoyo)
+                .OnComplete(() => {
+                    _spriteRenderer.color = Color.white;
+                });
+        //Destroy after die animation is done
         AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
         StartCoroutine(Death(stateInfo.length));
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
+        Vector3 hitDirection = (col.transform.position - transform.position).normalized;
         var proj = col.gameObject.GetComponent<Projectile>();
         if (proj != null && proj.args.DamageInfo.ownerType == ProjectileOwnerType.player)
         {
             Destroy(col.gameObject);
-            Hurt(1);
+            Hurt(1, hitDirection);
         }
     }
 
-    public void Hurt(int damage)
+    public void Hurt(int damage, Vector3 hitDirection = default)
     {
         string jsonValue = JsonUtility.ToJson(new CreateDamageTextEventArgs(transform.position, damage, DamageType.Normal));
         EventManager.Invoke("CreateDamageText", jsonValue);
         health -= damage;
         if (health <= 0)
         {
-            Kill();
+            Kill(hitDirection);
         }
     }
 
