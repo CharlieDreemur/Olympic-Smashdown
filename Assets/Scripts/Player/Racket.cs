c5a7bee09797122efc53166aa46f170caabbd5db
Roger Wang <rogerwang2520@gmail.com>
2023-03-10 22:24:35 -0600

    Tweak player racket cooldown

--- Assets/Scripts/Player/Racket.cs
+++ Assets/Scripts/Player/Racket.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Events;
 
